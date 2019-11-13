128c2c9518d6c3fdebb8bbdf06b2377fe8d769ea
rwev <rwev@protonmail.ch>
2019-11-13 16:46:22 -0700

    fix build

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,6 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-# If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'https://rwev.gitlab.io'
 RELATIVE_URLS = False
 
